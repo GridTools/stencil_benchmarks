3957b9ac137a73b1289a4455663f036d83f04067
Felix Thaler <thaler@cscs.ch>
2017-07-19 09:49:21 +0200

    added nontemportal hint to 1D stencils -> performance close to memcpy, updated plots

--- stencil_kernels_1D.h
+++ stencil_kernels_1D.h
@@ -3,6 +3,7 @@ void copy( T const* __restrict__ a,  T* __restrict__ b, const storage_info_t si,
     const int begin = si.index(h, h, h);
     const int end = si.index(h + isize, h + jsize, h + ksize);
 
+    #pragma vector nontemporal
     #pragma omp parallel for
     for (int i = begin; i < end; ++i) {
         b[i] = a[i];
@@ -15,6 +16,7 @@ void copyi1( T const* __restrict__ a,  T* __restrict__ b, const storage_info_t s
     const int end = si.index(h + isize, h + jsize, h + ksize);
     const int istride = si.template stride<0>();
 
+    #pragma vector nontemporal
     #pragma omp parallel for
     for (int i = begin; i < end; ++i) {
         b[i] = a[i+istride];
@@ -27,6 +29,7 @@ void sumi1( T const* __restrict__ a,  T* __restrict__ b, const storage_info_t si
     const int end = si.index(h + isize, h + jsize, h + ksize);
     const int istride = si.template stride<0>();
 
+    #pragma vector nontemporal
     #pragma omp parallel for
     for (int i = begin; i < end; ++i) {
         b[i] = a[i] + a[i+istride];
@@ -39,6 +42,7 @@ void avgi( T const* __restrict__ a,  T* __restrict__ b, const storage_info_t si,
     const int end = si.index(h + isize, h + jsize, h + ksize);
     const int istride = si.template stride<0>();
 
+    #pragma vector nontemporal
     #pragma omp parallel for
     for (int i = begin; i < end; ++i) {
         b[i] = a[i-istride] + a[i+istride];
@@ -51,6 +55,7 @@ void sumj1( T const* __restrict__ a,  T* __restrict__ b, const storage_info_t si
     const int end = si.index(h + isize, h + jsize, h + ksize);
     const int jstride = si.template stride<1>();
 
+    #pragma vector nontemporal
     #pragma omp parallel for
     for (int i = begin; i < end; ++i) {
         b[i] = a[i] + a[i+jstride];
@@ -63,6 +68,7 @@ void avgj( T const* __restrict__ a,  T* __restrict__ b, const storage_info_t si,
     const int end = si.index(h + isize, h + jsize, h + ksize);
     const int jstride = si.template stride<1>();
 
+    #pragma vector nontemporal
     #pragma omp parallel for
     for (int i = begin; i < end; ++i) {
         b[i] = a[i-jstride] + a[i+jstride];
@@ -75,6 +81,7 @@ void sumk1( T const* __restrict__ a,  T* __restrict__ b, const storage_info_t si
     const int end = si.index(h + isize, h + jsize, h + ksize);
     const int kstride = si.template stride<2>();
 
+    #pragma vector nontemporal
     #pragma omp parallel for
     for (int i = begin; i < end; ++i) {
         b[i] = a[i] + a[i+kstride];
@@ -87,6 +94,7 @@ void avgk( T const* __restrict__ a,  T* __restrict__ b, const storage_info_t si,
     const int end = si.index(h + isize, h + jsize, h + ksize);
     const int kstride = si.template stride<2>();
 
+    #pragma vector nontemporal
     #pragma omp parallel for
     for (int i = begin; i < end; ++i) {
         b[i] = a[i-kstride] + a[i+kstride];
@@ -101,6 +109,7 @@ void lap( T const* __restrict__ a,  T* __restrict__ b, const storage_info_t si, 
     const int istride = si.template stride<0>();
     const int jstride = si.template stride<1>();
 
+    #pragma vector nontemporal
     #pragma omp parallel for
     for (int i = begin; i < end; ++i) {
         b[i] = a[i] + a[i-istride] + a[i+istride] + a[i-jstride] + a[i+jstride];
