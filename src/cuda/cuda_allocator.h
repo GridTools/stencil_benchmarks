a163e9f503862294bd831ef6f3d037d62ffc875a
Felix Thaler <thaler@cscs.ch>
2017-08-09 10:30:11 +0200

    use unified memory

--- src/cuda/cuda_allocator.h
+++ src/cuda/cuda_allocator.h
@@ -20,13 +20,13 @@ namespace platform {
 
             value_type *allocate(std::size_t n) const {
                 value_type *ptr;
-                if (cudaMallocHost(reinterpret_cast<void **>(&ptr), n * sizeof(value_type)) != cudaSuccess)
+                if (cudaMallocManaged(reinterpret_cast<void **>(&ptr), n * sizeof(value_type)) != cudaSuccess)
                     throw ERROR("could not allocate pinned memory");
                 return ptr;
             }
 
             void deallocate(value_type *ptr, std::size_t) {
-                if (cudaFreeHost(ptr) != cudaSuccess)
+                if (cudaFree(ptr) != cudaSuccess)
                     throw ERROR("could not free memory");
             }
         };
--- src/cuda/cuda_variant.h
+++ src/cuda/cuda_variant.h
@@ -14,46 +14,13 @@ namespace platform {
           public:
             using value_type = ValueType;
 
-            cuda_variant(const arguments_map &args) : variant<Platform, ValueType>(args) {
-                value_type *d_src, *d_dst;
-                if (cudaMalloc(reinterpret_cast<void **>(&d_src), this->storage_size() * sizeof(value_type)) !=
-                    cudaSuccess)
-                    throw ERROR("could not allocate device memory");
-                if (cudaMalloc(reinterpret_cast<void **>(&d_dst), this->storage_size() * sizeof(value_type)) !=
-                    cudaSuccess)
-                    throw ERROR("could not allocate device memory");
-                this->set_ptrs(d_src + this->zero_offset(), d_dst + this->zero_offset());
-            }
+            cuda_variant(const arguments_map &args) : variant<Platform, ValueType>(args) {}
 
             virtual ~cuda_variant() {}
 
-            void prerun() override {
-                void *src_ptr = this->src() - this->zero_offset();
-                void *dst_ptr = this->dst() - this->zero_offset();
+            void prerun() override {}
 
-                if (cudaMemcpy(
-                        src_ptr, this->src_data(), this->storage_size() * sizeof(value_type), cudaMemcpyHostToDevice) !=
-                    cudaSuccess)
-                    throw ERROR("could not copy data to device");
-                if (cudaMemcpy(
-                        dst_ptr, this->dst_data(), this->storage_size() * sizeof(value_type), cudaMemcpyHostToDevice) !=
-                    cudaSuccess)
-                    throw ERROR("could not copy data to device");
-            }
-
-            void postrun() override {
-                void *src_ptr = this->src() - this->zero_offset();
-                void *dst_ptr = this->dst() - this->zero_offset();
-
-                if (cudaMemcpy(
-                        this->src_data(), src_ptr, this->storage_size() * sizeof(value_type), cudaMemcpyDeviceToHost) !=
-                    cudaSuccess)
-                    throw ERROR("could not copy data from device");
-                if (cudaMemcpy(
-                        this->dst_data(), dst_ptr, this->storage_size() * sizeof(value_type), cudaMemcpyDeviceToHost) !=
-                    cudaSuccess)
-                    throw ERROR("could not copy data from device");
-            }
+            void postrun() override {}
         };
 
     } // namespace cuda
