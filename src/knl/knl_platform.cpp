32c81417c015058678a865ec9756793f711bc93d
Felix Thaler <thaler@cscs.ch>
2017-08-09 10:30:48 +0200

    added ij-blocked variant to knl platform

--- src/knl/knl_platform.cpp
+++ src/knl/knl_platform.cpp
@@ -2,6 +2,7 @@
 
 #include "knl/knl_variant_1d.h"
 #include "knl/knl_variant_1d_nontemporal.h"
+#include "knl/knl_variant_ij_blocked.h"
 
 namespace platform {
 
@@ -11,6 +12,9 @@ namespace platform {
             arguments &pargs = args.command(name, "variant");
             pargs.command("1d");
             pargs.command("1d-nontemporal");
+            pargs.command("ij-blocked")
+                .add("i-blocksize", "block size in i-direction", "32")
+                .add("j-blocksize", "block size in j-direction", "8");
         }
 
         variant_base *flat::create_variant(const arguments_map &args) {
@@ -25,11 +29,15 @@ namespace platform {
                     return new variant_1d<flat, float>(args);
                 if (var == "1d-nontemporal")
                     return new variant_1d_nontemporal<flat, float>(args);
+                if (var == "ij-blocked")
+                    return new variant_ij_blocked<flat, float>(args);
             } else if (prec == "double") {
                 if (var == "1d")
                     return new variant_1d<flat, double>(args);
                 if (var == "1d-nontemporal")
                     return new variant_1d_nontemporal<flat, double>(args);
+                if (var == "ij-blocked")
+                    return new variant_ij_blocked<flat, double>(args);
             }
 
             return nullptr;
@@ -39,6 +47,9 @@ namespace platform {
             arguments &pargs = args.command(name, "variant");
             pargs.command("1d");
             pargs.command("1d-nontemporal");
+            pargs.command("ij-blocked")
+                .add("i-blocksize", "block size in i-direction", "32")
+                .add("j-blocksize", "block size in j-direction", "8");
         }
 
         variant_base *cache::create_variant(const arguments_map &args) {
@@ -53,11 +64,15 @@ namespace platform {
                     return new variant_1d<cache, float>(args);
                 if (var == "1d-nontemporal")
                     return new variant_1d_nontemporal<cache, float>(args);
+                if (var == "ij-blocked")
+                    return new variant_ij_blocked<cache, float>(args);
             } else if (prec == "double") {
                 if (var == "1d")
                     return new variant_1d<cache, double>(args);
                 if (var == "1d-nontemporal")
                     return new variant_1d_nontemporal<cache, double>(args);
+                if (var == "ij-blocked")
+                    return new variant_ij_blocked<cache, double>(args);
             }
 
             return nullptr;
