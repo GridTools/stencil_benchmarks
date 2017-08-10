9fb96191144f47b4032d2385dc8014729e4eacce
Felix Thaler <thaler@cscs.ch>
2017-08-10 15:34:17 +0200

    updated setup_perftools.sh for convenience

--- setup_perftools.sh
+++ setup_perftools.sh
@@ -2,4 +2,7 @@ module load perftools-base
 module load perftools
 
 export PAT_RT_EXPFILE_NAME=report
-export PAT_RT_PERFCTR=PAPI_L1_DCM,PAPI_TLB_DM
+export PAT_RT_PERFCTR=PAPI_L2_TCM,PAPI_TLB_DM
+export PAT_RT_EXPFILE_REPLACE=1
+
+alias rep='pat_report -T -s aggr_th=sum,sort_by_fu=yes -d P -b fu,th=HIDE report.xf | less'
