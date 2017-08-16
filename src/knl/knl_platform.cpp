482262e7dbb4697d854a760272e7158219b992d2
Felix Thaler <thaler@cscs.ch>
2017-08-16 09:22:05 +0200

    added variant ijk-blocked to knl platform

--- src/knl/knl_platform.cpp
+++ src/knl/knl_platform.cpp
@@ -3,80 +3,66 @@
 #include "knl/knl_variant_1d.h"
 #include "knl/knl_variant_1d_nontemporal.h"
 #include "knl/knl_variant_ij_blocked.h"
+#include "knl/knl_variant_ijk_blocked.h"
 
 namespace platform {
 
     namespace knl {
 
-        void flat::setup(arguments &args) {
-            arguments &pargs = args.command(name, "variant");
-            pargs.command("1d");
-            pargs.command("1d-nontemporal");
-            pargs.command("ij-blocked")
-                .add("i-blocksize", "block size in i-direction", "32")
-                .add("j-blocksize", "block size in j-direction", "8");
-        }
-
-        variant_base *flat::create_variant(const arguments_map &args) {
-            if (args.get("platform") != name)
-                return nullptr;
-
-            std::string prec = args.get("precision");
-            std::string var = args.get("variant");
-
-            if (prec == "single") {
-                if (var == "1d")
-                    return new variant_1d<flat, float>(args);
-                if (var == "1d-nontemporal")
-                    return new variant_1d_nontemporal<flat, float>(args);
-                if (var == "ij-blocked")
-                    return new variant_ij_blocked<flat, float>(args);
-            } else if (prec == "double") {
-                if (var == "1d")
-                    return new variant_1d<flat, double>(args);
-                if (var == "1d-nontemporal")
-                    return new variant_1d_nontemporal<flat, double>(args);
-                if (var == "ij-blocked")
-                    return new variant_ij_blocked<flat, double>(args);
+        namespace {
+            template <class Platform>
+            void common_setup(arguments &args) {
+                arguments &pargs = args.command(Platform::name, "variant");
+                pargs.command("1d");
+                pargs.command("1d-nontemporal");
+                pargs.command("ij-blocked")
+                    .add("i-blocksize", "block size in i-direction", "32")
+                    .add("j-blocksize", "block size in j-direction", "8");
+                pargs.command("ijk-blocked")
+                    .add("i-blocksize", "block size in i-direction", "32")
+                    .add("j-blocksize", "block size in j-direction", "8")
+                    .add("k-blocksize", "block size in k-direction", "8");
             }
 
-            return nullptr;
-        }
+            template <class Platform>
+            variant_base *common_create_variant(const arguments_map &args) {
+                if (args.get("platform") != Platform::name)
+                    return nullptr;
 
-        void cache::setup(arguments &args) {
-            arguments &pargs = args.command(name, "variant");
-            pargs.command("1d");
-            pargs.command("1d-nontemporal");
-            pargs.command("ij-blocked")
-                .add("i-blocksize", "block size in i-direction", "32")
-                .add("j-blocksize", "block size in j-direction", "8");
-        }
+                std::string prec = args.get("precision");
+                std::string var = args.get("variant");
 
-        variant_base *cache::create_variant(const arguments_map &args) {
-            if (args.get("platform") != name)
+                if (prec == "single") {
+                    if (var == "1d")
+                        return new variant_1d<Platform, float>(args);
+                    if (var == "1d-nontemporal")
+                        return new variant_1d_nontemporal<Platform, float>(args);
+                    if (var == "ij-blocked")
+                        return new variant_ij_blocked<Platform, float>(args);
+                    if (var == "ijk-blocked")
+                        return new variant_ijk_blocked<Platform, float>(args);
+                } else if (prec == "double") {
+                    if (var == "1d")
+                        return new variant_1d<Platform, double>(args);
+                    if (var == "1d-nontemporal")
+                        return new variant_1d_nontemporal<Platform, double>(args);
+                    if (var == "ij-blocked")
+                        return new variant_ij_blocked<Platform, double>(args);
+                    if (var == "ijk-blocked")
+                        return new variant_ijk_blocked<Platform, double>(args);
+                }
+
                 return nullptr;
-
-            std::string prec = args.get("precision");
-            std::string var = args.get("variant");
-
-            if (prec == "single") {
-                if (var == "1d")
-                    return new variant_1d<cache, float>(args);
-                if (var == "1d-nontemporal")
-                    return new variant_1d_nontemporal<cache, float>(args);
-                if (var == "ij-blocked")
-                    return new variant_ij_blocked<cache, float>(args);
-            } else if (prec == "double") {
-                if (var == "1d")
-                    return new variant_1d<cache, double>(args);
-                if (var == "1d-nontemporal")
-                    return new variant_1d_nontemporal<cache, double>(args);
-                if (var == "ij-blocked")
-                    return new variant_ij_blocked<cache, double>(args);
             }
-
-            return nullptr;
         }
+
+        void flat::setup(arguments &args) { common_setup<flat>(args); }
+
+        variant_base *flat::create_variant(const arguments_map &args) { return common_create_variant<flat>(args); }
+
+        void cache::setup(arguments &args) { common_setup<cache>(args); }
+
+        variant_base *cache::create_variant(const arguments_map &args) { return common_create_variant<cache>(args); }
 
     } // namespace knl
 
---
+++ src/knl/knl_variant_ijk_blocked.h
@@ -0,0 +1,79 @@
+#pragma once
+
+#include "knl/knl_variant.h"
+
+#define KERNEL(name, stmt)                                                                                      \
+    void name() override {                                                                                      \
+        const value_type *__restrict__ src = this->src();                                                       \
+        value_type *__restrict__ dst = this->dst();                                                             \
+        const int isize = this->isize();                                                                        \
+        const int jsize = this->jsize();                                                                        \
+        const int ksize = this->ksize();                                                                        \
+        constexpr int istride = 1;                                                                              \
+        const int jstride = this->jstride();                                                                    \
+        const int kstride = this->kstride();                                                                    \
+        if (this->istride() != 1)                                                                               \
+            throw ERROR("this variant is only compatible with unit i-stride layout");                           \
+                                                                                                                \
+        _Pragma("omp parallel for collapse(3)") for (int kb = 0; kb < ksize; kb += m_kblocksize) {              \
+            for (int jb = 0; jb < jsize; jb += m_jblocksize) {                                                  \
+                for (int ib = 0; ib < isize; ib += m_iblocksize) {                                              \
+                    const int imax = ib + m_iblocksize <= isize ? ib + m_iblocksize : isize;                    \
+                    const int jmax = jb + m_jblocksize <= jsize ? jb + m_jblocksize : jsize;                    \
+                    const int kmax = kb + m_kblocksize <= ksize ? kb + m_kblocksize : ksize;                    \
+                    int index = ib * istride + jb * jstride + kb * kstride;                                     \
+                                                                                                                \
+                    for (int k = kb; k < kmax; ++k) {                                                           \
+                        for (int j = jb; j < jmax; ++j) {                                                       \
+                            _Pragma("omp simd") _Pragma("vector nontemporal") for (int i = ib; i < imax; ++i) { \
+                                stmt;                                                                           \
+                                index += istride;                                                               \
+                            }                                                                                   \
+                            index += jstride - (imax - ib) * istride;                                           \
+                        }                                                                                       \
+                        index += kstride - (jmax - jb) * jstride;                                               \
+                    }                                                                                           \
+                }                                                                                               \
+            }                                                                                                   \
+        }                                                                                                       \
+    }
+
+namespace platform {
+
+    namespace knl {
+
+        template <class Platform, class ValueType>
+        class variant_ijk_blocked final : public knl_variant<Platform, ValueType> {
+          public:
+            using value_type = ValueType;
+
+            variant_ijk_blocked(const arguments_map &args)
+                : knl_variant<Platform, ValueType>(args), m_iblocksize(args.get<int>("i-blocksize")),
+                  m_jblocksize(args.get<int>("j-blocksize")), m_kblocksize(args.get<int>("k-blocksize")) {
+                if (m_iblocksize <= 0 || m_jblocksize <= 0)
+                    throw ERROR("invalid block size");
+            }
+
+            KERNEL(copy, dst[index] = src[index])
+            KERNEL(copyi, dst[index] = src[index + istride])
+            KERNEL(copyj, dst[index] = src[index + jstride])
+            KERNEL(copyk, dst[index] = src[index + kstride])
+            KERNEL(avgi, dst[index] = src[index - istride] + src[index + istride])
+            KERNEL(avgj, dst[index] = src[index - jstride] + src[index + jstride])
+            KERNEL(avgk, dst[index] = src[index - kstride] + src[index + kstride])
+            KERNEL(sumi, dst[index] = src[index] + src[index + istride])
+            KERNEL(sumj, dst[index] = src[index] + src[index + jstride])
+            KERNEL(sumk, dst[index] = src[index] + src[index + kstride])
+            KERNEL(lapij,
+                dst[index] = src[index] + src[index - istride] + src[index + istride] + src[index - jstride] +
+                             src[index + jstride])
+
+          private:
+            int m_iblocksize, m_jblocksize, m_kblocksize;
+        };
+
+    } // namespace knl
+
+} // namespace platform
+
+#undef KERNEL
