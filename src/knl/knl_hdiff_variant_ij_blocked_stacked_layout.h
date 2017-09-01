9dd2a9f67c78a53a6be30813519d361653302756
Stefan Moosbrugger <stefanmoosbrugger@gmail.com>
2017-09-01 14:06:55 +0200

    stacked layout tuning for knl

--- src/knl/knl_hdiff_variant_ij_blocked_stacked_layout.h
+++ src/knl/knl_hdiff_variant_ij_blocked_stacked_layout.h
@@ -39,6 +39,38 @@ namespace platform {
             value_type *flx_tmp() { return m_flx_tmp.data() + this->data_offset() + this->halo()*m_istride_tmp + this->halo()*m_kstride_tmp + this->halo()*m_jstride_tmp; }
             value_type *fly_tmp() { return m_fly_tmp.data() + this->data_offset() + this->halo()*m_istride_tmp + this->halo()*m_kstride_tmp + this->halo()*m_jstride_tmp; }
 
+            void prerun() override {
+                knl_hdiff_stencil_variant<Platform, ValueType>::prerun();
+                // copy data from in and coeff into block storage
+                const int istride = 1;
+                const int jstride = this->jstride();
+                const int kstride = this->kstride();
+                const int h = this->halo();
+                const int isize = this->isize();
+                const int jsize = this->jsize();
+                const int ksize = this->ksize();
+
+                for (int jb = 0; jb < m_nbj; ++jb) {
+                    for (int ib = 0; ib < m_nbi; ++ib) {
+                        const int bn = (jb*m_nbi + ib);
+                        const int imax = (ib+1)*m_iblocksize <= isize ? m_iblocksize : (isize - ib*m_iblocksize);
+                        const int jmax = (jb+1)*m_jblocksize <= jsize ? m_jblocksize : (jsize - jb*m_jblocksize);
+                        // iterate over k level for given block
+                        for (int k = -h; k < this->ksize()+h; ++k) {
+                            // iterate over i and j for given block
+                            for (int j = -h; j < jmax+h; ++j) {
+                                for (int i = -h; i < imax+h; ++i) {
+                                    const int index_tmp = (bn*this->ksize() + 2*bn*h)*m_kstride_tmp + k*m_kstride_tmp + i*m_istride_tmp + j*m_jstride_tmp;
+                                    const int index_real = ib*m_iblocksize*istride + jb*m_jblocksize*jstride + k*kstride + i*istride + j*jstride; 
+                                    in_tmp()[index_tmp] = this->in()[index_real];
+                                    coeff_tmp()[index_tmp] = this->coeff()[index_real];                            
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
             void hdiff() override {
 
                 const value_type *__restrict__ in = this->in();
@@ -61,32 +93,29 @@ namespace platform {
                 if (this->halo() < 2)
                     throw ERROR("Minimum required halo is 2");
                     
-                #pragma omp parallel for collapse(2) schedule(static, 1)
+                #pragma omp parallel for collapse(3) schedule(static, 1)
                 for (int k = 0; k < ksize; ++k) {
                     for (int jb = 0; jb < m_nbj; ++jb) {
                         for (int ib = 0; ib < m_nbi; ++ib) {
                             const int imax = (ib+1)*m_iblocksize <= isize ? m_iblocksize : (isize - ib*m_iblocksize);
                             const int jmax = (jb+1)*m_jblocksize <= jsize ? m_jblocksize : (jsize - jb*m_jblocksize);
 
-                            int index_lap = ib*m_iblocksize*istride + jb*m_jblocksize*jstride + k*kstride - istride - jstride;
-                            int index_flx = ib*m_iblocksize*istride + jb*m_jblocksize*jstride + k*kstride - istride;
-                            int index_fly = ib*m_iblocksize*istride + jb*m_jblocksize*jstride + k*kstride - jstride;
                             int index_out = ib*m_iblocksize*istride + jb*m_jblocksize*jstride + k*kstride;
+                            const int bn = (jb*m_nbi + ib);
                             
-                            int index_lap_tmp = (jb*m_nbi + ib)*ksize*m_kstride_tmp + k*m_kstride_tmp - m_istride_tmp - m_jstride_tmp;
-                            int index_flx_tmp = (jb*m_nbi + ib)*ksize*m_kstride_tmp + k*m_kstride_tmp - m_istride_tmp;
-                            int index_fly_tmp = (jb*m_nbi + ib)*ksize*m_kstride_tmp + k*m_kstride_tmp - m_jstride_tmp;
-                            int index_out_tmp = (jb*m_nbi + ib)*ksize*m_kstride_tmp + k*m_kstride_tmp;
+                            int index_lap_tmp = (bn*this->ksize() + 2*bn*h)*m_kstride_tmp + k*m_kstride_tmp - m_istride_tmp - m_jstride_tmp;
+                            int index_flx_tmp = (bn*this->ksize() + 2*bn*h)*m_kstride_tmp + k*m_kstride_tmp - m_istride_tmp;
+                            int index_fly_tmp = (bn*this->ksize() + 2*bn*h)*m_kstride_tmp + k*m_kstride_tmp - m_jstride_tmp;
+                            int index_out_tmp = (bn*this->ksize() + 2*bn*h)*m_kstride_tmp + k*m_kstride_tmp;
 
                             for (int j = 0; j < jmax+2; ++j) {
                                 #pragma omp simd
                                 for (int i = 0; i < imax+2; ++i) {
-                                    lap[index_lap_tmp] = 4 * in[index_lap] -
-                                        (in[index_lap - istride] + in[index_lap + istride] + in[index_lap - jstride] + in[index_lap + jstride]);
-                                    index_lap += istride;
+                                    lap[index_lap_tmp] = 4 * in[index_lap_tmp] -
+                                        (in[index_lap_tmp - m_istride_tmp] + in[index_lap_tmp + m_istride_tmp] + 
+                                         in[index_lap_tmp - m_jstride_tmp] + in[index_lap_tmp + m_jstride_tmp]);
                                     index_lap_tmp += m_istride_tmp;
                                 }
-                                index_lap += jstride - (imax+2) * istride;
                                 index_lap_tmp += m_jstride_tmp - (imax+2) * m_istride_tmp;
                             }
                             
@@ -94,12 +123,10 @@ namespace platform {
                                 #pragma omp simd
                                 for (int i = 0; i < imax+1; ++i) {
                                     flx[index_flx_tmp] = lap[index_flx_tmp + m_istride_tmp] - lap[index_flx_tmp];
-                                    if (flx[index_flx_tmp] * (in[index_flx + istride] - in[index_flx]) > 0)
+                                    if (flx[index_flx_tmp] * (in[index_flx_tmp + m_istride_tmp] - in[index_flx_tmp]) > 0)
                                         flx[index_flx_tmp] = 0.;
-                                    index_flx += istride;
                                     index_flx_tmp += m_istride_tmp;
                                 }
-                                index_flx += jstride - (imax+1) * istride;
                                 index_flx_tmp += m_jstride_tmp - (imax+1) * m_istride_tmp;
                             }
 
@@ -107,12 +134,10 @@ namespace platform {
                                 #pragma omp simd
                                 for (int i = 0; i < imax; ++i) {
                                     fly[index_fly_tmp] = lap[index_fly_tmp + m_jstride_tmp] - lap[index_fly_tmp];
-                                    if (fly[index_fly_tmp] * (in[index_fly + jstride] - in[index_fly]) > 0)
+                                    if (fly[index_fly_tmp] * (in[index_fly_tmp + m_jstride_tmp] - in[index_fly_tmp]) > 0)
                                         fly[index_fly_tmp] = 0.;
-                                    index_fly += istride;
                                     index_fly_tmp += m_istride_tmp;
                                 }
-                                index_fly += jstride - (imax) * istride;
                                 index_fly_tmp += m_jstride_tmp - (imax) * m_istride_tmp;
                             }
             
@@ -120,8 +145,9 @@ namespace platform {
                                 #pragma omp simd
                                 for (int i = 0; i < imax; ++i) {
                                     out[index_out] =
-                                        in[index_out] - coeff[index_out] *
-                                            (flx[index_out_tmp] - flx[index_out_tmp - m_istride_tmp] + fly[index_out_tmp] - fly[index_out_tmp - m_jstride_tmp]);
+                                        in[index_out_tmp] - coeff[index_out_tmp] *
+                                            (flx[index_out_tmp] - flx[index_out_tmp - m_istride_tmp] + 
+                                             fly[index_out_tmp] - fly[index_out_tmp - m_jstride_tmp]);
                                     index_out += istride;
                                     index_out_tmp += m_istride_tmp;
                                 }
