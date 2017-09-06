73e487755d991965d7edaa1c632e756d961aa9e0
Felix Thaler <thaler@cscs.ch>
2017-09-06 15:54:17 +0200

    offset allocated data for KNL to minimize L1 cache conflicts

--- src/knl/knl_allocator.h
+++ src/knl/knl_allocator.h
@@ -20,13 +20,28 @@ namespace platform {
             };
 
             value_type *allocate(std::size_t n) const {
-                value_type *ptr;
-                if (hbw_posix_memalign(reinterpret_cast<void **>(&ptr), alignment, n * sizeof(value_type)))
+                static std::size_t offset = 64;
+                char *raw_ptr;
+                if (hbw_posix_memalign(reinterpret_cast<void **>(&raw_ptr), alignment, n * sizeof(value_type) + offset))
                     throw ERROR("could not allocate HBW memory");
+
+                value_type *ptr = reinterpret_cast<value_type *>(raw_ptr + offset);
+
+                std::size_t *offset_ptr = reinterpret_cast<std::size_t *>(ptr) - 1;
+                *offset_ptr = offset;
+                if ((offset *= 2) >= 16384)
+                    offset = 64;
+
                 return ptr;
             }
 
-            void deallocate(value_type *ptr, std::size_t) { hbw_free(ptr); }
+            void deallocate(value_type *ptr, std::size_t) {
+                std::size_t *offset_ptr = reinterpret_cast<std::size_t *>(ptr) - 1;
+                std::size_t offset = *offset_ptr;
+
+                char *raw_ptr = reinterpret_cast<char *>(ptr) - offset;
+                hbw_free(raw_ptr);
+            }
         };
 
     } // namespace knl
