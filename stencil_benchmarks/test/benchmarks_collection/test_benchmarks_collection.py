329f8ea62e32de0ea61979a6af836b6d219236d2
Felix Thaler <thaler@cscs.ch>
2019-08-09 11:34:00 +0200

    Imrpoved test_benchmarks_collection

--- stencil_benchmarks/test/benchmarks_collection/test_benchmarks_collection.py
+++ stencil_benchmarks/test/benchmarks_collection/test_benchmarks_collection.py
@@ -6,4 +6,6 @@ from stencil_benchmarks import benchmark
 class TestAllBenchmarks(unittest.TestCase):
     def test_all(self):
         for bmark in benchmark.REGISTRY:
-            bmark().run()
+            with self.subTest(benchmark=bmark.__module__ + '.' +
+                              bmark.__name__):
+                bmark().run()
