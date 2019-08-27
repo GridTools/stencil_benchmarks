8ed1dc69653666b8d0875b54f20d5bc87c718e11
Felix Thaler <thaler@cscs.ch>
2019-08-27 11:38:43 +0200

    Use jinja FileSystemLoader to support template inheritance

--- stencil_benchmarks/tools/template.py
+++ stencil_benchmarks/tools/template.py
@@ -1,7 +1,10 @@
+import os
+
 import jinja2
 
 
 def render(template_file, **kwargs):
-    with open(template_file) as file:
-        contents = file.read()
-    return jinja2.Template(contents).render(**kwargs)
+    template_path, template_file = os.path.split(template_file)
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_path))
+    template = env.get_template(template_file)
+    return template.render(**kwargs)
