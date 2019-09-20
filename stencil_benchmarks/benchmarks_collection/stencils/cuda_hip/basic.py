91c149d58aa2c7416d0e1241cf29ada10afa9357
Felix Thaler <thaler@cscs.ch>
2019-09-20 14:43:02 +0200

    Fix basic stencils for float16

--- stencil_benchmarks/benchmarks_collection/stencils/cuda_hip/basic.py
+++ stencil_benchmarks/benchmarks_collection/stencils/cuda_hip/basic.py
@@ -18,14 +18,14 @@ class Copy(BasicStencilMixin, CopyStencil):
 class OnesidedAverage(BasicStencilMixin, OnesidedAverageStencil):
     def stencil_body(self):
         stride = self.strides[self.axis]
-        return f'out[index] = (inp[index] + inp[index + {stride}]) / 2;'
+        return f'out[index] = (inp[index] + inp[index + {stride}]) / {self.ctype_name}(2);'
 
 
 class SymmetricAverage(BasicStencilMixin, SymmetricAverageStencil):
     def stencil_body(self):
         stride = self.strides[self.axis]
         return (f'out[index] = (inp[index - {stride}] + '
-                f'inp[index + {stride}]) / 2;')
+                f'inp[index + {stride}]) / {self.ctype_name}(2);')
 
 
 class Laplacian(BasicStencilMixin, LaplacianStencil):
@@ -37,5 +37,5 @@ class Laplacian(BasicStencilMixin, LaplacianStencil):
             if apply_along_axis:
                 code.append(f'inp[index - {stride}] + inp[index + {stride}]')
 
-        return f'out[index] = {coeff} * inp[index] - (' + ' + '.join(
+        return f'out[index] = {self.ctype_name}({coeff}) * inp[index] - (' + ' + '.join(
             code) + ');'
