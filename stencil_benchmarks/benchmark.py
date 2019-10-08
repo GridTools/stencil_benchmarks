47193994b65bab866af88fc1e35ed62fb58ba519
Felix Thaler <thaler@cscs.ch>
2019-10-08 15:44:10 +0200

    Made benchmark callable

--- stencil_benchmarks/benchmark.py
+++ stencil_benchmarks/benchmark.py
@@ -154,6 +154,8 @@ class Benchmark(metaclass=BenchmarkMeta):
     @abc.abstractmethod
     def run(self):
         """Run the benchmark and return time."""
+    def __call__(self):
+        return self.run()
     def __repr__(self):
         return f'{type(self).__name__}(' + ', '.join(
             f'{param}={value!r}'
