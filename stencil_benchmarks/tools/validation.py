baa11335b992288a69738fea5d8e973b9551e7ef
Felix Thaler <thaler@cscs.ch>
2019-10-10 14:46:49 +0200

    Improved validation output for small domains

--- stencil_benchmarks/tools/validation.py
+++ stencil_benchmarks/tools/validation.py
@@ -1,3 +1,6 @@
+import textwrap
+
+import click
 import numpy as np
 
 
@@ -5,11 +8,8 @@ class ValidationError(RuntimeError):
     pass
 
 
-def check_equality(result, expected):
-    close = np.isclose(result, expected)
-    if np.all(close):
-        return
-    failing_indices = np.nonzero(~close)
+def _report_failures_large(result, expected):
+    failing_indices = np.nonzero(~np.isclose(result, expected))
     n_report = 20
     n_failures = 0
     for n_failures, index in enumerate(zip(*failing_indices)):
@@ -19,4 +19,34 @@ def check_equality(result, expected):
                   f'{result[index]:12.7f} != {expected[index]:12.7f}')
     if n_failures - n_report > 0:
         print(f'omitted further {n_failures - n_report} failures.')
+
+
+def _report_failures_small(result, expected):
+    assert result.ndim == expected.ndim == 3
+
+    def print_slice(data, correct):
+        fmt = '{:12.6g}'
+        for j in reversed(range(data.shape[1])):
+            for i in range(data.shape[0]):
+                click.echo(click.style(fmt.format(data[i, j]),
+                                       fg='green' if correct[i, j] else 'red'),
+                           nl=False)
+            click.echo()
+
+    for k in range(expected.shape[2]):
+        correct = np.isclose(result[:, :, k], expected[:, :, k])
+        print(f'result[:, :, {k}]:')
+        print_slice(result[:, :, k], correct)
+        print(f'expected[:, :, {k}]:')
+        print_slice(expected[:, :, k], correct)
+
+
+def check_equality(result, expected):
+    close = np.isclose(result, expected)
+    if np.all(close):
+        return
+    if result.ndim != 3 or np.product(result.shape) > 1000:
+        _report_failures_large(result, expected)
+    else:
+        _report_failures_small(result, expected)
     raise ValidationError()
