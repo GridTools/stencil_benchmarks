65b9a8bea36b0350c7e914dd41b2e13bdc4d9cb5
Felix Thaler <thaler@cscs.ch>
2020-02-13 01:00:05 +0900

    Improved prefetching

--- stencil_benchmarks/benchmarks_collection/stencils/openmp/vertical_advection.py
+++ stencil_benchmarks/benchmarks_collection/stencils/openmp/vertical_advection.py
@@ -1,9 +1,14 @@
 from .mixin import VerticalAdvectionMixin
 from ..base import VerticalAdvectionStencil
+from .... import benchmark
 
 
 class KInnermost(VerticalAdvectionMixin, VerticalAdvectionStencil):
-    pass
+    prefetch_distance = benchmark.Parameter('vertical prefetching distance', 0)
+
+    def template_args(self):
+        return dict(**super().template_args(),
+                    prefetch_distance=self.prefetch_distance)
 
 
 class KMiddle(VerticalAdvectionMixin, VerticalAdvectionStencil):
