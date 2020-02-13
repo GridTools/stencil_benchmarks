52158ef02d58267ebd4e3e43545e4419ef4e265b
Felix Thaler <thaler@cscs.ch>
2020-02-13 13:40:43 +0100

    Vadv k-innermost-vec

--- stencil_benchmarks/benchmarks_collection/stencils/openmp/vertical_advection.py
+++ stencil_benchmarks/benchmarks_collection/stencils/openmp/vertical_advection.py
@@ -11,5 +11,12 @@ class KInnermost(VerticalAdvectionMixin, VerticalAdvectionStencil):
                     prefetch_distance=self.prefetch_distance)
 
 
+class KInnermostVec(KInnermost):
+    vector_size = benchmark.Parameter('vector size', 16)
+
+    def template_args(self):
+        return dict(**super().template_args(), vector_size=self.vector_size)
+
+
 class KMiddle(VerticalAdvectionMixin, VerticalAdvectionStencil):
     pass
