ea589020a44c0fc04c0596d824d36092ec2e57ee
Felix Thaler <thaler@cscs.ch>
2020-02-14 14:08:32 +0100

    sbench-openmp-collection

--- stencil_benchmarks/scripts/sbench_openmp_collection.py
+++ stencil_benchmarks/scripts/sbench_openmp_collection.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
-import math
-
 import click
+import numpy as np
 
-from stencil_benchmarks.benchmarks_collection.stencils.openmp import basic
+from stencil_benchmarks.benchmarks_collection.stencils.openmp import (
+    basic, horizontal_diffusion as hdiff, vertical_advection as vadv)
 from stencil_benchmarks.tools.multirun import (Configuration,
-                                               run_scaling_benchmark)
+                                               run_scaling_benchmark,
+                                               truncate_block_size_to_domain)
 from stencil_benchmarks.tools import alloc
 
 
@@ -15,12 +16,13 @@ def main():
     pass
 
 
-def common_kwargs(options=None, **kwargs):
+def common_kwargs(options=None, **overrides):
     kwargs = dict(platform_preset='none',
                   alignment=alloc.l1_dcache_linesize(),
-                  **kwargs)
+                  verify=False)
+    kwargs.update(overrides)
     for o in options:
-        name, value = o.split('=')
+        name, value = o.split('=', 1)
         name = name.replace('-', '_')
         try:
             value = bool(value)
@@ -41,33 +43,101 @@ def common_kwargs(options=None, **kwargs):
 @click.option('--executions', '-e', type=int, default=101)
 @click.option('--dtype', '-d', default='float32')
 @click.option('--option', '-o', multiple=True)
-@click.option('--domain', type=int, nargs=3, default=(1024, 1024, 80))
-def blocked_copy(output, executions, dtype, option, domain):
-    kwargs = common_kwargs(option, dtype=dtype, loop='3D', halo=1)
+def basic_bandwidth(output, executions, dtype, option):
+    kwargs = common_kwargs(option,
+                           dtype=dtype,
+                           loop='3D-blocked',
+                           halo=1,
+                           block_size=(1024, 16, 1))
 
     stream_kwargs = kwargs.copy()
     stream_kwargs.update(loop='1D', halo=0)
 
     configurations = [
         Configuration(basic.Copy, name='stream', **stream_kwargs),
+        Configuration(basic.Copy, name='copy', **kwargs),
+        Configuration(basic.OnesidedAverage, name='avg-i', axis=0, **kwargs),
+        Configuration(basic.OnesidedAverage, name='avg-j', axis=1, **kwargs),
+        Configuration(basic.OnesidedAverage, name='avg-k', axis=2, **kwargs),
+        Configuration(basic.SymmetricAverage,
+                      name='sym-avg-i',
+                      axis=0,
+                      **kwargs),
+        Configuration(basic.SymmetricAverage,
+                      name='sym-avg-j',
+                      axis=1,
+                      **kwargs),
+        Configuration(basic.SymmetricAverage,
+                      name='sym-avg-k',
+                      axis=2,
+                      **kwargs),
+        Configuration(basic.Laplacian,
+                      name='lap-ij',
+                      along_x=True,
+                      along_y=True,
+                      along_z=False,
+                      **kwargs)
+    ]
+    table = run_scaling_benchmark(
+        configurations,
+        executions,
+        preprocess_args=truncate_block_size_to_domain)
+    table.to_csv(output)
+
+
+@main.command()
+@click.argument('output', type=click.Path())
+@click.option('--executions', '-e', type=int, default=101)
+@click.option('--dtype', '-d', default='float32')
+@click.option('--option', '-o', multiple=True)
+def horizontal_diffusion_bandwidth(output, executions, dtype, option):
+    vector_size = 64 // np.dtype(dtype).itemsize
+    kwargs = common_kwargs(option,
+                           dtype=dtype,
+                           alignment=64,
+                           vector_size=vector_size)
+
+    configurations = [
+        Configuration(hdiff.ClassicVec, **kwargs, block_size=(1024, 16, 1)),
+        Configuration(hdiff.OnTheFlyVec, **kwargs, block_size=(1024, 8, 1)),
+        Configuration(hdiff.MinimumMem, **kwargs, block_size=(1024, 64, 1))
     ]
 
-    def blocks(d):
-        for i in range(int(math.log2(d))):
-            yield 2**i
+    table = run_scaling_benchmark(
+        configurations,
+        executions,
+        preprocess_args=truncate_block_size_to_domain)
+    table.to_csv(output)
 
-    for bx in blocks(domain[0]):
-        for by in blocks(domain[1]):
-            for bz in blocks(domain[2]):
-                configurations.append(
-                    Configuration(basic.Copy,
-                                  name=f'blocked-{bx}-{by}-{bz}',
-                                  block_size=(bx, by, bz),
-                                  **kwargs))
 
-    table = run_scaling_benchmark(configurations,
-                                  executions,
-                                  domain_range=[domain])
+@main.command()
+@click.argument('output', type=click.Path())
+@click.option('--executions', '-e', type=int, default=101)
+@click.option('--dtype', '-d', default='float32')
+@click.option('--option', '-o', multiple=True)
+def vertical_advection_bandwidth(output, executions, dtype, option):
+    vector_size = 64 // np.dtype(dtype).itemsize
+    kwargs = common_kwargs(option,
+                           dtype=dtype,
+                           layout=(2, 0, 1),
+                           vector_size=vector_size)
+
+    configurations = [
+        Configuration(vadv.KMiddleVec, **kwargs, block_size=(1024, 1)),
+        Configuration(vadv.KInnermostVec,
+                      **kwargs,
+                      block_size=(64, 1),
+                      prefetch_distance=4),
+        Configuration(vadv.KInnermostBlockVec,
+                      **kwargs,
+                      block_size=(64, 1),
+                      prefetch_distance=2)
+    ]
+
+    table = run_scaling_benchmark(
+        configurations,
+        executions,
+        preprocess_args=truncate_block_size_to_domain)
     table.to_csv(output)
 
 
