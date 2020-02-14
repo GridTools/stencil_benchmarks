142b5ffe628d4f2cbe8cfc27e8a17a04240e0249
Felix Thaler <thaler@cscs.ch>
2020-02-14 14:55:36 +0100

    Fixed unmapping of normal pages, correct handling of huge page size

--- stencil_benchmarks/tools/alloc.cpp
+++ stencil_benchmarks/tools/alloc.cpp
@@ -1,6 +1,9 @@
 #include <cstdlib>
 #include <cstring>
+#include <fstream>
 #include <memory>
+#include <regex>
+#include <string>
 
 #include <sys/mman.h>
 #include <unistd.h>
@@ -22,10 +25,21 @@ struct malloc_buffer {
   std::size_t size;
 };
 
+std::size_t get_huge_page_size() {
+  std::ifstream meminfo("/proc/meminfo");
+  std::regex hugepagesize_regex("Hugepagesize: *([0-9]+) kB$");
+
+  std::string line;
+  while (std::getline(meminfo, line)) {
+    std::smatch match;
+    if (std::regex_match(line, match, hugepagesize_regex))
+      return std::stoll(match[1].str()) * 1024;
+  }
+  return 0;
+}
+
 struct mmap_deleter {
   void operator()(void *ptr) const {
-    // TODO: get correct huge page size (currently assume 2MB)
-    constexpr std::size_t page_size = 2 * 1024 * 1024;
     auto paged_size = (size + page_size - 1) / page_size * page_size;
     if (munmap(ptr, paged_size)) {
       auto err = errno;
@@ -35,20 +49,29 @@ struct mmap_deleter {
   }
 
   std::size_t size = 0;
+  std::size_t page_size;
 };
 
 struct mmap_buffer {
   mmap_buffer(std::size_t size, bool huge_pages) {
+    static const std::size_t huge_page_size = get_huge_page_size();
+    static const std::size_t normal_page_size = sysconf(_SC_PAGESIZE);
     auto flags = MAP_PRIVATE | MAP_ANONYMOUS | MAP_NORESERVE;
-    if (huge_pages)
+    std::size_t page_size = normal_page_size;
+    if (huge_pages) {
+      if (!huge_page_size)
+        throw std::runtime_error("could not get huge page size");
       flags |= MAP_HUGETLB;
+      page_size = huge_page_size;
+    }
     void *ptr = mmap(0, size, PROT_READ | PROT_WRITE, flags, -1, 0);
     if (ptr == MAP_FAILED) {
       auto err = errno;
       throw std::runtime_error(std::string("mmap failed with error ") +
                                strerror(err));
     }
-    data = std::unique_ptr<void, mmap_deleter>(ptr, mmap_deleter{size});
+    data =
+        std::unique_ptr<void, mmap_deleter>(ptr, mmap_deleter{size, page_size});
   }
 
   std::unique_ptr<void, mmap_deleter> data;
