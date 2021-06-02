0a8ca5fa4f39850b9f1bd1a200124fcfbb2f6aee
Felix Thaler <thaler@cscs.ch>
2021-06-02 08:38:28 +0200

    Vectorized openmp-blocked HDIFF

--- stencil_benchmarks/benchmarks_collection/stencils/openmp_blocked/horizontal_diffusion.py
+++ stencil_benchmarks/benchmarks_collection/stencils/openmp_blocked/horizontal_diffusion.py
@@ -32,60 +32,9 @@
 # SPDX-License-Identifier: BSD-3-Clause
 from stencil_benchmarks.benchmarks_collection.stencils import base
 
-from .basic import BasicStencilMixin
+from .mixin import StencilMixin
 
 
-class OnTheFly(BasicStencilMixin, base.HorizontalDiffusionStencil):
-    def stencil_body(self):
-        return f'''const auto inp_ij = inp[{self.index(0, 0, 0)}];
-                const auto inp_im1j = inp[{self.index(-1, 0, 0)}];
-                const auto inp_ip1j = inp[{self.index(1, 0, 0)}];
-                const auto inp_ijm1 = inp[{self.index(0, -1, 0)}];
-                const auto inp_ijp1 = inp[{self.index(0, 1, 0)}];
-                const auto inp_im2j = inp[{self.index(-2, 0, 0)}];
-                const auto inp_im1jm1 = inp[{self.index(-1, -1, 0)}];
-                const auto inp_im1jp1 = inp[{self.index(-1, 1, 0)}];
-                const auto inp_ip2j = inp[{self.index(2, 0, 0)}];
-                const auto inp_ip1jm1 = inp[{self.index(1, -1, 0)}];
-                const auto inp_ip1jp1 = inp[{self.index(1, 1, 0)}];
-                const auto inp_ijm2 = inp[{self.index(0, -2, 0)}];
-                const auto inp_ijp2 = inp[{self.index(0, 2, 0)}];
-
-                const auto lap_ij = 4 * inp_ij - inp_im1j
-                                               - inp_ip1j
-                                               - inp_ijm1
-                                               - inp_ijp1;
-                const auto lap_imj = 4 * inp_im1j - inp_im2j
-                                                  - inp_ij
-                                                  - inp_im1jm1
-                                                  - inp_im1jp1;
-                const auto lap_ipj = 4 * inp_ip1j - inp_ij
-                                                  - inp_ip2j
-                                                  - inp_ip1jm1
-                                                  - inp_ip1jp1;
-                const auto lap_ijm = 4 * inp_ijm1 - inp_im1jm1
-                                                  - inp_ip1jm1
-                                                  - inp_ijm2
-                                                  - inp_ij;
-                const auto lap_ijp = 4 * inp_ijp1 - inp_im1jp1
-                                                  - inp_ip1jp1
-                                                  - inp_ij
-                                                  - inp_ijp2;
-
-                auto flx_ij = lap_ipj - lap_ij;
-                flx_ij = flx_ij * (inp_ip1j - inp_ij) > 0 ? 0 : flx_ij;
-
-                auto flx_imj = lap_ij - lap_imj;
-                flx_imj = flx_imj * (inp_ij - inp_im1j) > 0 ? 0 : flx_imj;
-
-                auto fly_ij = lap_ijp - lap_ij;
-                fly_ij = fly_ij * (inp_ijp1 - inp_ij) > 0 ? 0 : fly_ij;
-
-                auto fly_ijm = lap_ij - lap_ijm;
-                fly_ijm = fly_ijm * (inp_ij - inp_ijm1) > 0 ? 0 : fly_ijm;
-
-                out[{self.index(0, 0, 0)}] = inp_ij
-                                           - coeff[{self.index(0, 0, 0)}] * (
-                                                      flx_ij - flx_imj +
-                                                      fly_ij - fly_ijm);
-                '''
+class OnTheFly(StencilMixin, base.HorizontalDiffusionStencil):
+    def template_file(self):
+        return 'horizontal_diffusion.j2'
