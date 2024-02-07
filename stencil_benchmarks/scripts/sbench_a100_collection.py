142820be94c51d068e7b78c7c0dee740fd131f62
Ioannis Magkanaris <ioannis.magkanaris@cscs.ch>
2024-02-07 12:53:39 +0100

    Enabled code printing

--- stencil_benchmarks/scripts/sbench_a100_collection.py
+++ stencil_benchmarks/scripts/sbench_a100_collection.py
@@ -52,7 +52,8 @@ common_kwargs = default_kwargs(backend='cuda',
                                verify=False,
                                dry_runs=1,
                                alignment=128,
-                               dtype='float32')
+                               dtype='float32',
+                               print_code=True)
 
 
 @main.command()
